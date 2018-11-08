d1cecefe9740df0d339e1a411e76d500c236045c
landon <sbshz85@gmail.com>
2018-11-08 10:25:56 +0900

    first test

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+import Login from "./components/Login";
+
+class Main extends Component {
+  render() {
+    return (
+      <Login />
+    )
+  }
+}
+ 
+export default Main;
+ 
+const styles = StyleSheet.create({});
+ 
